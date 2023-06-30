670b2617f8ed4328ddeca6161637415d62bc6cdd
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-06-30 18:49:55 -0400

    attempt to optimize

--- app/day/[day]/page.tsx
+++ app/day/[day]/page.tsx
@@ -1,6 +1,7 @@
 import { allPosts } from 'contentlayer/generated'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import Link from 'next/link'
+import { useMemo } from 'react'
 import Container from 'util/containers'
 
 export async function generateStaticParams() {
@@ -14,15 +15,18 @@ const notFoundPost = allPosts.find(
 )
 
 export default function Page({ params }: { params: { day: string } }) {
-  const post =
-    allPosts.find(
-      (post) =>
-        post._raw.flattenedPath ===
-        `posts/${params.day.replaceAll('posts%2F', '')}`,
-    ) || notFoundPost
+  const post = useMemo(
+    () =>
+      allPosts.find(
+        (post) =>
+          post._raw.flattenedPath ===
+          `posts/${params.day.replaceAll('posts%2F', '')}`,
+      ) || notFoundPost,
+    [params.day],
+  )
 
-  const previousDay = Number(params.day) - 1
-  const nextDay = Number(params.day) + 1
+  const previousDay = useMemo(() => Number(params.day) - 1, [params.day])
+  const nextDay = useMemo(() => Number(params.day) + 1, [params.day])
   const MDXContent = useMDXComponent(post?.body.code || '')
 
   return (
--- components/Gallery/CarouselButton.tsx
+++ components/Gallery/CarouselButton.tsx
@@ -44,9 +44,9 @@ export const CarouselButton = ({
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d={paths}
           ></path>
         </svg>
