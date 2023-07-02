8d1749c485d79def60c13d5c43ce92c5d358f544
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-02 17:55:46 -0400

    oops

---
+++ components/Header/AboutDropdown.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { Menu } from '@headlessui/react'
+import React from 'react'
+import { useTheme } from 'next-themes'
+import { ACCENT_TEXT_CLASS } from '@/util/consts'
+
+export function AboutDropdown({
+  name,
+  children,
+}: {
+  name: string
+  children: React.ReactNode
+}) {
+  const { theme } = useTheme()
+
+  return (
+    <Menu>
+      <Menu.Button className={`hover:${ACCENT_TEXT_CLASS}`}>{name}</Menu.Button>
+      <Menu.Items className="absolute mt-2">
+        <div className="grid grid-cols-1 px-2 py-2 space-y-2 rounded-lg dark:bg-gray-700 bg-gray-200">
+          {React.Children.map(children, (child) => (
+            <Menu.Item>{child}</Menu.Item>
+          ))}
+        </div>
+      </Menu.Items>
+    </Menu>
+  )
+}
