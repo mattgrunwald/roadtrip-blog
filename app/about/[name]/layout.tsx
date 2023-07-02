2a96831582ac5a23b85edd18ad7b4c1a836b6c08
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-02 18:42:01 -0400

    mobile style fixes

--- app/about/[name]/layout.tsx
+++ app/about/[name]/layout.tsx
@@ -2,7 +2,7 @@ import Container from 'util/containers'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-2xl mx-auto py-10 px-4">
+    <div className="max-w-2xl mx-auto px-4">
       <Container.Text>{children}</Container.Text>
     </div>
   )
