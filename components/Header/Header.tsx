418dfa22c9ba23dab19219c15e2b01d81c2efdce
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-03 18:15:08 -0400

    don't get clever with server component styling

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import { AboutDropdown } from './AboutDropdown'
 import { ModeToggle } from '../ModeToggle'
-import { ACCENT_TEXT_CLASS } from '@/util/consts'
 
 export default function Header() {
   return (
@@ -10,16 +9,16 @@ export default function Header() {
         <b>Roadtrek Trip 2023</b>
         <div className="flex items-center justify-around">
           <nav className="ml-auto max-md:mr-4 md:mr-6 text-sm font-medium space-x-6 pr">
-            <Link href="/" className={`hover:${ACCENT_TEXT_CLASS}`}>
+            <Link href="/" className="hover:text-red-400">
               Home
             </Link>
           </nav>
           <span className="max-md:mr-4 md:mr-6 mr-6 text-sm font-medium">
             <AboutDropdown name="About">
-              <Link href="/about/van" className={`hover:${ACCENT_TEXT_CLASS}`}>
+              <Link href="/about/van" className="hover:text-red-400">
                 The Van
               </Link>
-              <Link href="/about/site" className={`hover:${ACCENT_TEXT_CLASS}`}>
+              <Link href="/about/site" className="hover:text-red-400">
                 This Site
               </Link>
             </AboutDropdown>
