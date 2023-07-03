bea11821535c0cffb724dbad9fd2c732beabc81d
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-02 20:11:54 -0400

    give all about images same width and height

--- util/Image.tsx
+++ util/Image.tsx
@@ -1,2 +1,4 @@
 import NextImage from 'next/image'
-export const Image = (props: any) => <NextImage {...props} />
+export const Image = (props: any) => (
+  <NextImage height={800} width={800} {...props} />
+)
