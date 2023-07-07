eebd788959a8d1e268f1ae7540b519bed614b2c8
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-07 11:52:35 -0400

    use tailwind for all styles

--- components/TripMap.tsx
+++ components/TripMap.tsx
@@ -30,12 +30,17 @@ const MapMarker = ({
   onClick = () => {},
 }: MapMarkerProps) => (
   <Marker coordinates={coordinates} onClick={onClick}>
-    <circle r={5} fill={ACCENT_COLOR} strokeWidth={2} opacity={opacity} />
+    <circle
+      r={5}
+      className={name === '' ? 'hover:cursor-pointer' : ''}
+      fill={ACCENT_COLOR}
+      strokeWidth={2}
+      opacity={opacity}
+    />
     <text
       textAnchor="middle"
       y={offset}
-      className={`font-semibold dark:fill-gray-200 fill-gray-700`}
-      style={{ fontFamily: 'system-ui', zIndex: -1 }}
+      className={`font-semibold dark:fill-gray-200 fill-gray-700 -z-10`}
     >
       {name || ''}
     </text>
