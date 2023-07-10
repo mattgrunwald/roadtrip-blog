26e2358a1a594ad9b8e87782fe97ebc4000865b5
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-10 00:26:39 -0400

    Toc (#1)
    * add table of contents to about pages
    
    * add mobile dropdown
    
    * bug fixes
    
    * add chevron icons
    
    * make mobile toc absolute and animated
    
    * parse headers in helper
    
    * use headers directly
    
    * accomodate lg pages
    
    * better styling for large screens
    
    * cleaner layout

--- app/about/[name]/layout.tsx
+++ app/about/[name]/layout.tsx
@@ -1,9 +1,9 @@
-import Container from 'util/containers'
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-2xl mx-auto">
-      <Container.Text>{children}</Container.Text>
-    </div>
+    <>
+      <div className="grid max-md:grid-cols-1 md:grid-cols-[1fr,3fr] 2xl:grid-cols-[1fr,2fr,1fr] 3xl:grid-cols-[1fr,1fr,1fr] gap-x-10 gap-y-5">
+        {children}
+      </div>
+    </>
   )
 }
