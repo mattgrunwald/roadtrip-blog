e9c7783eb3b0f7fdd28c53f76be8d4b8c45fa404
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-07-10 00:41:33 -0400

    three colums for large screens

--- app/about/[name]/layout.tsx
+++ app/about/[name]/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="grid max-md:grid-cols-1 md:grid-cols-[1fr,3fr] 2xl:grid-cols-[1fr,2fr,1fr] 3xl:grid-cols-[1fr,1fr,1fr] gap-x-10 gap-y-5">
+      <div className="grid max-md:grid-cols-1 md:grid-cols-[1fr,3fr] xl:grid-cols-[1fr,2fr,1fr] 3xl:grid-cols-[1fr,1fr,1fr] gap-x-10 gap-y-5">
         {children}
       </div>
     </>
