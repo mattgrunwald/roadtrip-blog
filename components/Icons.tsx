2be9ee88ee0d7ebd7057769c555a0e919476d898
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-11-22 17:17:47 -0500

    day link button for image wall images (#28)
    * day link button for image wall images'
    
    * show link when image loads

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -45,15 +45,16 @@ const Contentlayer = () => (
   </svg>
 )
 
-const Link = () => (
+const Link = ({ stroke = '#f87171', opacity = 1 }) => (
   <svg
     className={className}
     fill="none"
-    stroke="#f87171"
+    stroke={stroke}
     strokeLinecap="round"
     strokeLinejoin="round"
     strokeWidth={2}
     viewBox="0 0 24 24"
+    opacity={opacity}
   >
     <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6M15 3h6v6M10 14L21 3" />
   </svg>
--- util/contentlayer-helpers.ts
+++ util/contentlayer-helpers.ts
@@ -7,7 +7,7 @@ import { AboutPage } from '@/.contentlayer/generated'
 export type GalleryImageSource = {
   src: string
   preview: string
-  day: string
+  day: number
   /**
    * height / width
    */
@@ -33,7 +33,7 @@ export async function convertImages(
       src,
       preview,
       ratio,
-      day: String(day),
+      day: Number(day),
     })
   }
   return res
