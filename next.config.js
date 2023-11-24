4d77d6f503e8be7e7f650d6d8be48ab3df633dd9
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-11-24 15:22:44 -0500

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const { withContentlayer } = require('next-contentlayer')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -6,4 +9,6 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withBundleAnalyzer({
+  ...withContentlayer(nextConfig),
+})
