07f68cbc32fd68cf83b318ba9334da5af1f94cf6
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-12-14 16:58:16 -0500

    don't compute image size using js

--- components/ImageWall/ImageWallImage.tsx
+++ components/ImageWall/ImageWallImage.tsx
@@ -1,52 +1,43 @@
 import { GalleryImageSource, Size } from '@/util/types'
 import Image from 'next/image'
 import { DayLink } from './DayLink'
-import { useMemo, useState } from 'react'
 
 export type ImageWallImageProps = {
   image: GalleryImageSource
-  baseWidth: number
   onClick: () => void
 }
 
 export default function ImageWallImage({
   image,
-  baseWidth,
   onClick,
 }: ImageWallImageProps) {
-  const [loaded, setLoaded] = useState(false)
-
-  const srcSizes = useMemo(() => {
-    switch (image.size) {
-      case Size.Wide:
-        return '(max-width: 1024px) 100vw, 50vw'
-      default:
-        return '(max-width: 1024px) 50vw, 25vw'
-    }
-  }, [image])
-
-  const [width, height] = useMemo(
-    () => [baseWidth * image.colSpan, baseWidth * image.colSpan * image.ratio],
-    [baseWidth, image.colSpan, image.ratio],
-  )
-
+  const srcSizes =
+    image.size === Size.Wide
+      ? '(max-width: 1024px) 100vw, 50vw'
+      : '(max-width: 1024px) 50vw, 25vw'
   return (
     <div
       className={`flex justify-center relative row-span-${image.rowSpan} col-span-${image.colSpan}`}
     >
-      {loaded && <DayLink day={image.day} />}
-      <Image
-        className={`hover:cursor-zoom-in object-cover`}
-        src={image.src}
-        width={width}
-        height={height}
-        placeholder="blur"
-        blurDataURL={image.preview}
-        alt=""
-        onClick={onClick}
-        sizes={srcSizes}
-        onLoadingComplete={() => setLoaded(true)}
-      />
+      <div
+        className={`${
+          image.size === Size.Tall
+            ? 'min-h-[72vw] lg:min-h-[36vw]'
+            : 'min-h-[36vw] lg:min-h-[18vw]'
+        }`}
+      >
+        <DayLink day={image.day} />
+        <Image
+          className={`hover:cursor-zoom-in object-cover`}
+          src={image.src}
+          fill
+          placeholder="blur"
+          blurDataURL={image.preview}
+          alt=""
+          onClick={onClick}
+          sizes={srcSizes}
+        />
+      </div>
     </div>
   )
 }
