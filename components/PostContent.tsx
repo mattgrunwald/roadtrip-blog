9c423a1a62dfd5c5d39188c09e37ae2558607157
Matt Grunwald <matt.grunwald.dev@gmail.com>
2023-12-29 02:24:56 -0500

    Vqa (#63)
    * remove padding above image gallery
    
    * use index.tsx for trip map

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -1,6 +1,6 @@
 import Gallery from '@/components/Gallery'
 import TripCalendar from '@/components/TripCalendar'
-import TripMap from '@/components/TripMap/TripMap'
+import TripMap from '@/components/TripMap'
 import { Marker, Post } from 'contentlayer/generated'
 import Container from '@/util/containers'
 import { useMemo } from 'react'
