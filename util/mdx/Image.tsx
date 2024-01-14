66f2838be6352ebf4a13a651e6768a8b98528eca
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-01-14 17:04:05 -0500

    add more images and acks to about van page

--- util/mdx/Image.tsx
+++ util/mdx/Image.tsx
@@ -1,5 +1,13 @@
 import NextImage from 'next/image'
 export const Image = (props: any) => {
   const sizes = '(max-width:640px) 100vw, (max-width: 1535px) 700px, 800px'
-  return <NextImage height={800} width={800} sizes={sizes} {...props} />
+  const { ack, ...imageProps } = props
+  return (
+    <figure>
+      <NextImage height={800} width={800} sizes={sizes} {...imageProps} />
+      {ack && (
+        <figcaption className="text-right">{`Source: ${ack}`}</figcaption>
+      )}
+    </figure>
+  )
 }
