9aca84235695b63e4ce249a9b26149a2d9b4a489
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-04-01 10:19:21 -0400

    suppress hydration warning for theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`bg-slate-50 text-slate-900 antialiased dark:bg-slate-950 dark:text-slate-50 ${inter.className} `}
       >
