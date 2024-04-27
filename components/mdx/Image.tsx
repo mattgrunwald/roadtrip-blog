303bc59d3284a0add1984f902c94f197bf61d1e1
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-04-27 13:18:39 -0400

    use gpu for about images

--- components/mdx/Image.tsx
+++ components/mdx/Image.tsx
@@ -1,7 +1,8 @@
 import { IMAGE_QUALITY } from '@/util/consts'
 import NextImage from 'next/image'
 export const Image = (props: any) => {
-  const { ack, small, ...imageProps } = props
+  const { ack, small, className, ...imageProps } = props
+  const updatedClassName = `${className} translate-x-0 translate-y-0 transform-gpu`
   const sizes = small
     ? '(max-width:640px) 100vw, 700px'
     : '(max-width:640px) 100vw, (max-width: 1535px) 700px, 800px'
@@ -12,6 +13,7 @@ export const Image = (props: any) => {
         width={800}
         quality={IMAGE_QUALITY}
         sizes={sizes}
+        className={updatedClassName}
         {...imageProps}
       />
       {ack && (
