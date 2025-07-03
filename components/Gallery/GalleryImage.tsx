1ae3b56e7df9e79c05bb6811274faa538254a483
Matt Grunwald <matt.grunwald.dev@gmail.com>
2025-07-03 01:45:47 -0400

    Update GalleryImage.tsx

--- components/Gallery/GalleryImage.tsx
+++ components/Gallery/GalleryImage.tsx
@@ -42,7 +42,7 @@ export const GalleryImage = ({
   const sharedProps = {
     fill: true,
     sizes: sizeSet,
-    quality: modal ? 80 : IMAGE_QUALITY,
+    quality: modal ? 100 : IMAGE_QUALITY,
     priority: first,
   }
 
