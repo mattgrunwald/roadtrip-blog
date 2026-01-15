4cdadfbf9c031ff62617b9b6a09c83b6c2c82340
Matt Grunwald <matt.grunwald.dev@gmail.com>
2026-01-15 12:09:44 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { withContentlayer } = require('next-contentlayer2')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 2678400,
+  },
 }
 
 module.exports = withContentlayer(nextConfig)
